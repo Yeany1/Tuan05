c889414f9afd1ee9447872ea42d6da5bd99c95b2
khanh260802 <dinhbaoyeny2002@gmail.com>
2023-10-18 02:44:19 +0700

    fix loi

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { SafeAreaView } from 'react-native-web';
+import TongHaiSo from './screens/TongHaiSo'
+import A from './screens/A';
+import B from './screens/B';
+import C from './screens/C';
+import Tiki from './screens/Tiki';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      {/* <TongHaiSo /> */}
+      {/* <A /> */}
+      <B />
+      {/* <C /> */}
+      {/* <Tiki /> */}
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
